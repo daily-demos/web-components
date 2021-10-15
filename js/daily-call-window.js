d9f3482aa8169bd115f3d93f85ef1636f3863664
Ashley Blewer <ashley.blewer@gmail.com>
2021-10-15 16:07:32 -0400

    Base toggle on callObject source instead of local state

--- js/daily-call-window.js
+++ js/daily-call-window.js
@@ -2,14 +2,15 @@ const template = document.createElement('template');
 
 template.innerHTML = `
   <style>
-  :host {
-    background-color: #121a24;
-  }
-  ::slotted(video) {
-    display: block;
-    max-width: 100%;
-    max-height: 100%;
-  }
+    :host {
+      background-color: #121a24;
+      overflow: hidden;
+    }
+    ::slotted(video) {
+      object-fit: cover;
+      width: 100%;
+      height: 100%;
+    }
   </style>
   <slot></slot>
   `;
